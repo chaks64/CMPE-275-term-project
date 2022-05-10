8a35008459b24d35d45a379fa7e215802c106f7c
chaks64 <chintanbiren.shah@sjsu.edu>
2022-05-10 13:38:51 -0700

    merge1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,20 +9,14 @@ import Verify from "./components/Verify/Verify";
 import Error from "./components/Verify/Error";
 import GoogleSignup from "./components/GoogleSignUp/GoogleSignUp";
 import Home from "./components/Home/Home";
-<<<<<<< Updated upstream
 import CreateEvent from "./components/CreateEvent/CreateEvent";
 
 export const ThemeContext = React.createContext();
-=======
-import EventCard from "./components/EventCard/EventCard";
-
->>>>>>> Stashed changes
 
 function App() {
   const [systemTime, setSystemTime] = useState(new Date());
   const [mimicTime, toggleMimicTime] = useState(false);
   return (
-<<<<<<< Updated upstream
     <ThemeContext.Provider
       value={{ systemTime, setSystemTime, mimicTime, toggleMimicTime }}
     >
@@ -37,19 +31,6 @@ function App() {
         <Route path="/createEvent" element={<CreateEvent />} />
       </Routes>
     </ThemeContext.Provider>
-=======
-    <Routes>
-      <Route path="/" element={<NavBar />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/verify" element={<Verify />} />
-      <Route path="/error" element={<Error />} />
-      <Route path="/googleSignup" element={<GoogleSignup />} />
-      <Route path="/home" element={<Home />} />
-        <Route path="/event/details" element={<EventCard />} />
-
-    </Routes>
->>>>>>> Stashed changes
   );
 }
 
